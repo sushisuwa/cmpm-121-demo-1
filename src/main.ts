487c1a6e01f198daa48f0c1ef63abfff5394f941
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2024-10-17 20:39:17 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,13 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let num_clicks = 0;
+const button = document.createElement("button");
+button.innerHTML = "Click to count the lemons";
+button.onclick = () => {
+  num_clicks += 1;
+  button.innerHTML = `${num_clicks} 🍋 so far`;
+};
+
+app.append(button);
