060a70af85ce0c5119acba92387a41f853b1149e
sushisuwa <jsuwa@ucsc.edu>
2024-10-25 00:09:35 -0700

    Step 9 completed

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,16 @@ interface Item {
 }
 
 const availableItems: Item[] = [
-  { name: "Lemon Expert", cost: 10, rate: 0.1, purchased: 0 },
-  { name: "Lemonade Stand", cost: 100, rate: 2, purchased: 0 },
-  { name: "Lemon Union", cost: 1000, rate: 50, purchased: 0 },
+  { name: "🍋🥼 Lemon Expert", cost: 10, rate: 0.1, purchased: 0 },
+  { name: "🍋🏠 Lemonade Stand", cost: 100, rate: 2, purchased: 0 },
+  { name: "🍋👥 Lemon Union", cost: 1000, rate: 50, purchased: 0 },
 ];
 
+const itemElements: HTMLParagraphElement[] = [];
+const itemButtonElements: HTMLButtonElement[] = [];
+
 let lastUpdateTime = performance.now();
-//let autoClick = true;
-//let autoClick1 = true;
-//let autoClick2 = true;
 let upgradeModifier = 0;
-/*let item1 = 0,
-  item2 = 0,
-  item3 = 0;
-let priceAuto = 10,
-  priceSuper = 100,
-  priceUltra = 1000;
-  */
 const priceMod = 1.15;
 const gameName = "Zesty Quest: Count of the Lemons";
 document.title = gameName;
@@ -40,14 +33,13 @@ app.append(body);
 const growthRate = document.createElement("p");
 app.append(growthRate);
 
-const autoClickerPurchased = document.createElement("p");
-app.append(autoClickerPurchased);
+availableItems.forEach((item) => {
+  const item_purchased = document.createElement("p");
+  item_purchased.innerHTML = `${item.purchased} : ${item.name}s`;
+  app.append(item_purchased);
 
-const superPurchased = document.createElement("p");
-app.append(superPurchased);
-
-const ultraPurchased = document.createElement("p");
-app.append(ultraPurchased);
+  itemElements.push(item_purchased);
+});
 
 const buttonContainer = document.createElement("div");
 buttonContainer.className = "button-container";
@@ -56,15 +48,16 @@ app.append(buttonContainer);
 let num_lemon = 0;
 function incrementLemon(amount: number) {
   num_lemon += amount;
-
   body.innerHTML = `you have ${Math.floor(num_lemon)} 🍋 in your basket`;
-  availableItems.forEach((item) => {
-    const item_purchased = document.createElement("p");
-    item_purchased.innerHTML = `${item.purchased} : ${item.name}s`;
+  availableItems.forEach((item, index) => {
+    itemElements[index].innerHTML = `${item.purchased} : ${item.name}s`;
+
+    if(num_lemon >= item.cost){
+      itemButtonElements[index].disabled = false;
+      }else{itemButtonElements[index].disabled = true;}
+    
+    itemButtonElements[index].innerHTML = `Purchase ${item.name}, Cost: ${Math.floor(item.cost)}`;
   });
-  //autoClickerPurchased.innerHTML = `${item1} : 🥼 Lemon Experts`;
-  //superPurchased.innerHTML = `${item2} : 🫙 Lemonade Stands`;
-  //ultraPurchased.innerHTML = `${item3} : 👩‍👩‍👧‍👦 Lemon Unions `;
   growthRate.innerHTML = `${upgradeModifier.toFixed(1)}🍋/s`;
 }
 
@@ -82,53 +75,18 @@ buttonContainer.append(button);
 
 availableItems.forEach((item) => {
   const button = document.createElement("button");
-  button.innerHTML = `Purchase ${item.name}`;
+  button.innerHTML = `Purchase ${item.name}, Cost: ${item.cost}`;
 
   button.addEventListener("click", () => {
     num_lemon -= item.cost;
-    item.rate += item.rate;
     upgradeModifier += item.rate;
 
     item.purchased += 1;
     item.cost = item.cost * priceMod;
   });
 
+  itemButtonElements.push(button);
   app.append(button);
+  button.disabled = true;
 });
 requestAnimationFrame(animate);
-
-/*
-const upgrade = document.createElement("button");
-upgrade.innerHTML = "Purchase Lemon Expert 🥼";
-upgrade.addEventListener("click", () => {
-  num_lemon = num_lemon - priceAuto;
-  upgradeModifier += 0.1;
-  item1 += 1;
-  priceAuto = priceAuto * priceMod;
-});
-app.append(upgrade);
-const upgradeSuper = document.createElement("button");
-upgradeSuper.innerHTML = "Purchase 🫙 Lemonade Stand";
-upgradeSuper.addEventListener("click", () => {
-  num_lemon = num_lemon - priceSuper;
-  upgradeModifier += 1;
-  item2 += 1;
-  priceSuper = priceSuper * priceMod;
-});
-app.append(upgradeSuper);
-
-const upgradeUltra = document.createElement("button");
-upgradeUltra.innerHTML = "Purchase Lemon Union 👩‍👩‍👧‍👦";
-upgradeUltra.addEventListener("click", () => {
-  num_lemon = num_lemon - priceUltra;
-  upgradeModifier += 50;
-  item3 += 1;
-  priceUltra = priceUltra * priceMod;
-});
-app.append(upgradeUltra);
-
-console.log(autoClick);
-upgrade.disabled = autoClick;
-upgradeSuper.disabled = autoClick1;
-upgradeUltra.disabled = autoClick2;
-*/
